9c1834a7877d4a4043c338c04b4bc7b594e30197
riccardo1-saporiti <126947811+riccardo1-saporiti@users.noreply.github.com>
2023-07-01 14:52:23 +0200

    Update addpath_geopdes.m

--- addpath_geopdes.m
+++ addpath_geopdes.m
@@ -1,4 +1,4 @@
     addpath (genpath ('nurbs-1.4.3'))
-    addpath (genpath ('geopdes-3.2.2'))
-    addpath (genpath ('geopdes_hierarchical-3.2.2'))
-    addpath (genpath (['/Users/mns/Desktop/EPFL/MATLAB/GeoPDEs-full/geopdes-3.2.2/geopdes/inst/Test_Dirichlet_Feature/useful_functions']))
+    addpath (genpath ('geopdes'))
+    addpath (genpath ('geopdes_hierarchical'))
+    addpath (genpath (['/Users/.../GeoPDEs-full/geopdes/inst/Test_Dirichlet_Feature/useful_functions']))
