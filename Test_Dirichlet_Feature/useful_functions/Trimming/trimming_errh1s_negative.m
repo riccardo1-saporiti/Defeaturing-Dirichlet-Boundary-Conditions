7e51a52a516d2818f8c520a4155a40c4a8cecee1
riccardo1-saporiti <126947811+riccardo1-saporiti@users.noreply.github.com>
2023-07-01 13:57:48 +0200

    Delete Test_Dirichlet_Feature/useful_functions/Trimming directory

--- Test_Dirichlet_Feature/useful_functions/Trimming/trimming_errh1s_negative.m
+++
@@ -1,6 +0,0 @@
-function errh1s = trimming_errh1s_negative (u_0, msh_cart, space, reparam, u_ex)
-
-A_gradu_gradv = op_gradu_gradv_trimming(space, space, msh_cart, reparam);
-errh1s = sqrt((u_0-u_ex)'*A_gradu_gradv*(u_0-u_ex));
-
-end
